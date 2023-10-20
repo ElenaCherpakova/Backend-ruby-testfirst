35c6120615cdf52a12f70196be81de298ab50252
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-19 22:35:42 -0400

    replace reduce to inject

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -8,13 +8,11 @@ def subtract(num1, num2)
 end
 
 def sum(array)
-  array.reduce(0) { |sum, num| sum + num }
+  array.inject(0) { |sum, num| sum + num }
 end
 
 def multiply(*numbers)
-  result = 1
-  numbers.each { |number| result *= number }
-  result
+  numbers.inject(1) { |result, number| result * number }
 end
 
 def power(num, pwr)
@@ -22,7 +20,12 @@ def power(num, pwr)
 end
 
 def factorial(num)
-  return 1 if num.zero?
-
-  num * factorial(num - 1)
+  num.downto(1).inject(1) { |result, num| result * num }
 end
+
+# def factorial(num)
+#   return 1 if num.zero?
+
+#   num * factorial(num - 1)
+# end
+
