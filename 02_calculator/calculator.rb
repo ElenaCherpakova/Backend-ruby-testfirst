bbf8d21a8a0af5778730a8b48bc035d5cb4c9408
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-19 21:28:17 -0400

    calculator

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,28 @@
-#write your code here
+# write your code here
+def add(num1, num2)
+  num1 + num2
+end
+
+def subtract(num1, num2)
+  num1 - num2
+end
+
+def sum(array)
+  array.reduce(0) { |sum, num| sum + num }
+end
+
+def multiply(*numbers)
+  result = 1
+  numbers.each { |number| result *= number }
+  result
+end
+
+def power(num, pwr)
+  num**pwr
+end
+
+def factorial(num)
+  return 1 if num.zero?
+
+  num * factorial(num - 1)
+end
