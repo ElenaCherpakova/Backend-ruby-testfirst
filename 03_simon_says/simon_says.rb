9da4f1376a2017c62e438a50713d3e0fc9f3823f
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-23 18:34:08 -0400

    finish 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,26 @@
-#write your code here
+# write your code here
+
+def echo(input)
+  input
+end
+
+def shout(input)
+  input.upcase
+end
+
+def repeat(input, times = 2)
+  ([input] * times).join(' ')
+end
+
+def start_of_word(input, num)
+  input.slice(0, num)
+end
+
+def first_word(input)
+  input.split(' ').first
+end
+
+def titleize(input)
+  words = %w[and over the]
+  input.capitalize.split(' ').map { |word| words.include?(word) ? word : word.capitalize }.join(' ')
+end
