81ebc5868b91cd2b07a7f33bbc748ef4cc7e4132
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-23 23:59:12 -0400

    04, 05 and 06 tests

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,29 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def time_string
+    time = Time.at(@seconds).utc
+    # puts "Time: #{time}"
+    time.strftime('%H:%M:%S')
+  end
+
+  # 2nd option by using padded helper function
+  # def padded(convert_time)
+  #   convert_time.to_s.rjust(2, '0')
+  # end
+
+  # def time_string
+  #   hours = @seconds / 3600
+  #   minutes = (@seconds % 3600) / 60
+  #   seconds = (@seconds % 3600) % 60
+  #   "#{padded(hours)}:#{padded(minutes)}:#{padded(seconds)}"
+  # end
 end
+
+# timer = Timer.new
+# puts timer.seconds = 4000
+# puts timer.time_string
