9a64f8c455083ded99ec1c9df8031468f6594008
ElenaCherpakova <elena.cherpakova@gmail.com>
2023-10-24 00:01:11 -0400

    05 book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,16 @@
+# class Book
 class Book
-# write your code here
+  attr_accessor :title
+
+  def initialize(title = '')
+    @title = title
+  end
+
+  def title=(new_title)
+    not_capitalize_words = %w[the a an and in of]
+    capitalized = new_title.capitalize.split(' ').map do |word|
+      not_capitalize_words.include?(word) ? word : word.capitalize
+    end
+    @title = capitalized.join(' ')
+  end
 end
